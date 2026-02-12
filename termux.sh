00af27e7d0a4ed2daf1edcdc942ccefff89e4209
Faust <faddist25@gmail.com>
2026-02-13 04:08:05 +0800

    Update git clone URLs to use HTTPS

--- termux.sh
+++ termux.sh
@@ -7,11 +7,11 @@ function install_zsh() {
 	fi
 	if [ ! -d "$HOME/.oh-my-zsh" ]; then
 		echo -e "\\e[32m[ oh-my-zsh ]\\e[m clonning repository"
-		git clone git://github.com/robbyrussell/oh-my-zsh.git "$HOME/.oh-my-zsh" --depth 1 --quiet >/dev/null
+		git clone https://github.com/robbyrussell/oh-my-zsh.git "$HOME/.oh-my-zsh" --depth 1 --quiet >/dev/null
 	else
 		if ask "\\e[32m[ oh-my-zsh ]\\e[m configs found, overwrite?" Y; then
 			rm -rf "$HOME/.oh-my-zsh"
-			git clone git://github.com/robbyrussell/oh-my-zsh.git "$HOME/.oh-my-zsh" --depth 1 --quiet >/dev/null
+			git clone https://github.com/robbyrussell/oh-my-zsh.git "$HOME/.oh-my-zsh" --depth 1 --quiet >/dev/null
 		fi
 	fi
 	curl -fsLo "$HOME/.oh-my-zsh/themes/lambda-mod.zsh-theme" https://cdn.rawgit.com/onlurking/termux/master/.termux/lambda-mod.zsh-theme
@@ -20,7 +20,7 @@ function install_zsh() {
 	if [ ! -d "$HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting" ]; then
 		if ask "\\e[32m[ oh-my-zsh ]\\e[m enable syntax highlighting?" Y; then
 			echo -e "\\e[32m[ oh-my-zsh ]\\e[m downloading plugin"
-			git clone git://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting" --quiet >/dev/null
+			git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting" --quiet >/dev/null
 		fi
 	else
 		sed -i '4s/.*/plugins=(git)/' "$HOME/.zshrc"
@@ -29,7 +29,7 @@ function install_zsh() {
 	if [ ! -d "$HOME/.oh-my-zsh/plugins/zsh-autosuggestions" ]; then
 		if ask "\\e[32m[ oh-my-zsh ]\\e[m enable autosuggestions?" Y; then
 			echo -e "\\e[32m[ oh-my-zsh ]\\e[m downloading plugin"
-			git clone git://github.com/zsh-users/zsh-autosuggestions.git "$HOME/.oh-my-zsh/plugins/zsh-autosuggestions" --quiet >/dev/null
+			git clone https://github.com/zsh-users/zsh-autosuggestions.git "$HOME/.oh-my-zsh/plugins/zsh-autosuggestions" --quiet >/dev/null
 			sed -i '4s/git/git zsh-autosuggestions/' "$HOME/.zshrc"
 		fi
 	fi
